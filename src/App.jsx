68d16e6caa475213d38810b42314262240989099
Luis Gutiérrez <luisghtz@outlook.com>
2021-07-04 11:29:29 -0500

    Move Elements to independent files

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,6 @@
-import styled from 'styled-components';
-
-const MyHeader = styled.h1`
-  font-family: cursive;
-  color: red;
-`;
-
-const MySection = styled.section`
-  padding: 0.5rem;
-  border: 1rem solid purple;
-`;
-
-const MyButton = styled.button`
-  font-family: cursive;
-  font-size: 1.3rem;
-  border: 1px dashed;
-  border-radius: 5px;
-  padding: 8px 10px;
-  cursor: pointer;
-  &:hover {
-    background-color: black;
-    color: white;
-    transition: all 0.2s;
-  }
-`;
+import MyButton from 'elements/MyButton';
+import MyHeader from 'elements/MyHeader';
+import MySection from 'elements/MySection';
 
 function App() {
   return (
