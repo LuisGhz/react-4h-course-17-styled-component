643570ded4e222cf7c76e56181c92bf08a109c61
Luis Gutiérrez <luisghtz@outlook.com>
2021-07-04 11:34:13 -0500

    Set color based on props.primary

--- src/elements/MyButton.jsx
+++ src/elements/MyButton.jsx
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 
 export default styled.button`
-  font-family: cursive;
-  font-size: 1.3rem;
+  color: ${ props => props.primary ? 'blue' : 'red' };
+  cursor: pointer;
   border: 1px dashed;
   border-radius: 5px;
+  font-family: cursive;
+  font-size: 1.3rem;
   padding: 8px 10px;
-  cursor: pointer;
   &:hover {
     background-color: black;
     color: white;
