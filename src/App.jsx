73b5f02d4e6c36aadfe6bd6421902e28d4ed8ac6
Luis Gutiérrez <luisghtz@outlook.com>
2021-07-04 12:15:41 -0500

    Import ThemeProvider from styled Set GreenTheme to ThemeProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
+import { ThemeProvider } from 'styled-components';
 import MyButton from 'elements/MyButton';
 import MyHeader from 'elements/MyHeader';
 import MySection from 'elements/MySection';
 import MaterialButton from 'elements/MaterialButton'
+import MyButtonTheme from 'elements/MyButtonTheme';
+import { GreenTheme } from 'theme/theme';
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={ GreenTheme } >
       <MySection>
         <MyHeader>My header</MyHeader>
         <MyButton primary >My button</MyButton>
         <MyButton>My button</MyButton>
+        <MyButtonTheme>My button</MyButtonTheme>
         <MaterialButton>Material button</MaterialButton>
       </MySection>
-    </div>
+    </ThemeProvider>
   );
 }
 
