ecceddf0fff38efd1e004fd740e779368542b005
Luis Gutiérrez <luisghtz@outlook.com>
2021-07-04 11:24:19 -0500

    Create elements with styled components Create h1 called MyHeader Create section called MySection Create button called MyButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
+import styled from 'styled-components';
+
+const MyHeader = styled.h1`
+  font-family: cursive;
+  color: red;
+`;
+
+const MySection = styled.section`
+  padding: 0.5rem;
+  border: 1rem solid purple;
+`;
+
+const MyButton = styled.button`
+  font-family: cursive;
+  font-size: 1.3rem;
+  border: 1px dashed;
+  border-radius: 5px;
+  padding: 8px 10px;
+  cursor: pointer;
+  &:hover {
+    background-color: black;
+    color: white;
+    transition: all 0.2s;
+  }
+`;
 
 function App() {
   return (
     <div className="App">
-
+      <MySection>
+        <MyHeader>My header</MyHeader>
+        <MyButton>My button</MyButton>
+      </MySection>
     </div>
   );
 }
